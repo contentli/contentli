9e488c6b25a918b365da39dafec450fa8dbebff1
Kjell-Magnus Vike <magnus@vike.se>
2019-04-16 22:41:53 +0200

    Namespace fix and db_url support

--- config/database.php
+++ config/database.php
@@ -61,11 +61,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => env("DATABASE_URL") ? parse_url(env("DATABASE_URL"))["host"] : '127.0.0.1',
+            'port' => env("DATABASE_URL") ? parse_url(env("DATABASE_URL"))["port"] : '5432',
+            'database' => env("DATABASE_URL") ? substr(parse_url(env("DATABASE_URL"))["path"], 1) : 'forge',
+            'username' => env("DATABASE_URL") ? parse_url(env("DATABASE_URL"))["user"] : 'forge',
+            'password' => env("DATABASE_URL") ? parse_url(env("DATABASE_URL"))["pass"] : '',
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
